c1f58d200ca8df5d3d519a2553e821e44e0ac472
xwp <xwp_fullstack@163.com>
2018-05-28 14:18:01 +0800

    Initial commit

---
+++ Dailyfresh/urls.py
@@ -0,0 +1,31 @@
+"""Dailyfresh URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+
+    # 用户模块
+    url(r'^users/',include('apps.users.urls',namespace='users')),
+    # 订单模块
+    url(r'^orders/',include('apps.orders.urls',namespace='orders')),
+    # 购物车模块
+    url(r'^cart/',include('apps.cart.urls',namespace='cart')),
+    # 商品模块，当所有的url不匹配时，才会匹配这句
+    url(r'^',include('apps.goods.urls',namespace='goods')),
+]
